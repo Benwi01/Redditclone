f38de1d974934caedf1cda78109ac6f6e5de43b4
benjamin wiese <benjamin060101@gmail.com>
2024-10-19 11:30:25 +0200

    idk

--- Redditcloneblazor/obj/Debug/net8.0/Redditcloneblazor.AssemblyInfo.cs
+++ Redditcloneblazor/obj/Debug/net8.0/Redditcloneblazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Redditcloneblazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4617e3f019a8ec755dae18baefe9d519394df8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c57433b654a5b08b59dfcc57d70a4dc040a9baa2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Redditcloneblazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Redditcloneblazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
